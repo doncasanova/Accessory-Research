fe7cfa3b0b756ede0ab783ab5fee4a7009bf20cf
doncasanova <ethanjuan@yahoo.com>
2018-07-29 14:41:56 -0500

    adding Amazon ad nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import AmazonB from "./components/AmazonB";
 
 const App = () => (
   <Router>
     <div>
       <Nav />
+      <AmazonB/>
       <Switch>
         <Route exact path="/" component={Books} />
         <Route exact path="/books" component={Books} />
---
+++ client/src/components/AmazonB/AmazonB.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const AmazonB = () => (
+    <div
+    className="alignleft">
+     <script type="text/javascript">
+       	amzn_assoc_ad_type = "banner";
+	amzn_assoc_marketplace = "amazon";
+	amzn_assoc_region = "US";
+	amzn_assoc_placement = "assoc_banner_placement_default";
+	amzn_assoc_campaigns = "amazonhomepage";
+	amzn_assoc_banner_type = "rotating";
+	amzn_assoc_p = "12";
+	amzn_assoc_width = "300";
+	amzn_assoc_height = "250";
+	amzn_assoc_tracking_id = "staticbgone-20";
+	amzn_assoc_linkid = "c4ff4fad646f34f210308ce0f8c92ca9";
+     </script>
+     <script src="//z-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&Operation=GetScript&ID=OneJS&WS=1"></script>
+    </div>
+  );
+  
+  export default AmazonB;
