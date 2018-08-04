87d500f9871b32b46cf4c1cb8b5206b27882dbf0
doncasanova <ethanjuan@yahoo.com>
2018-08-04 12:09:39 -0500

    react-create-app on Heroku

---
+++ client/build/images/images.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+
+const Image = () => (
+    <div>
+        <img className="m-2" src="http://accessoryresearch.com/Film%20Cleaner.JPG" Height = "200px" alt="Film Cleaner"></img>
+        <img className="m-2" src="http://accessoryresearch.com/Plastic.JPG" Height = "200px" alt="Plastic Cleaner"></img>
+        <img className="m-2" src="http://accessoryresearch.com/Screen.JPG" Height = "200px" alt="Screan Cleaner"></img>
+        <img className="m-2" src="http://accessoryresearch.com/Heavy%20Duty.JPG" Height = "200px" alt="Heavy Cleaner"></img>
+        <img className="m-2" src="http://accessoryresearch.com/Glass%20Cleaner.JPG" Height = "200px" alt="Glass Cleaner"></img>
+    </div>
+  );
+  
+  export default Image;
+  
