6ec35185338a7d2a68f9567c8a3a9a4e5f1d589e
doncasanova <ethanjuan@yahoo.com>
2018-08-11 12:53:40 -0500

    redue heroku

--- client/src/components/ProductScrollDon/ProductScrollDon.js
+++ client/src/components/ProductScrollDon/ProductScrollDon.js
@@ -7,7 +7,7 @@ export default class ProductScrollDon extends React.Component {
     return (
 
       <div className="donsCSS">
-        <nav id="navbar-example2" className="navbar navbar-light bg-light navTest fixed-top">
+        <nav id="navbar-example2" className="navbar navbar-light bg-light navTest">
           <a className="navbar-brand" href="#">Navbar</a>
           <ul className="nav nav-pills">
             <li className="nav-item">
@@ -28,33 +28,33 @@ export default class ProductScrollDon extends React.Component {
           </ul>
         </nav>
         <div data-spy="scroll" data-target="#navbar-example2" data-offset="0">
-          <div className="row test1">
-            <h4 id="fat">
+          <div id="fat" className="row test1">
+            <h4 >
           fat 
             </h4>
             <img className="m-2" src="https://media.giphy.com/media/Jacgl87d9azte/giphy.gif" height = "200px" alt="Office Space Static"></img>
           </div>
-          <div className="row test2">
+          <div id="mdo" className="row test2">
             <h4 id="mdo" className="">
            mdo
             </h4>
             <img className="m-2" src="https://media.giphy.com/media/3ohjV978XBXKrgROSI/giphy.gif" height = "200px" alt="Office Space Static"></img>
             
           </div>
-          <div className="row test3">
+          <div id="one" className="row test3">
             <h4 id="one" className="">
          one
             </h4>
             <img className="m-2" src="https://tse2.mm.bing.net/th?id=OIP.RO7U-jwdpwlzf1SqR7Yx9gHaFj&pid=15.1&P=0&w=215&h=162" height = "200px" alt="Office Space Static"></img>
           </div>
-          <div className="row test4">
-            <h4 id="two" className="">
+          <div id="two" className="row test4">
+            <h4 className="">
              two
             </h4>
             <img className="m-2" src="https://thumbs.gfycat.com/ThreadbareFatJaeger-max-1mb.gif" height = "200px" alt="Office Space Static"></img>
           </div>
-          <div className="row test5">
-            <h4 id="three" className="">
+          <div id="three" className="row test5">
+            <h4  className="">
             three
             </h4>
             <img className="m-2" src="https://38.media.tumblr.com/aa1a5169b4d38b4b83d779b79afbe8e9/tumblr_mkzqk0rXPD1s8yy9co1_500.gif" height = "200px" alt="Office Space Static"></img>
