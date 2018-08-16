00fdc4b871a177610744c2fc027c97a11bbeb45f
doncasanova <ethanjuan@yahoo.com>
2018-08-16 13:57:09 -0500

    cropped images to make oz and qt images and add button for create account

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -52,6 +52,7 @@ class Nav extends Component {
 
           </ul>
            <Login/>
+            <a href="/profile/create"> <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Create Account</button></a>
           {/* <form className="form-inline my-2 my-lg-0">
             <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
             <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
