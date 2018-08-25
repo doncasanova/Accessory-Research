59c6245f8331bef51ccae8394859d7dccfe26494
doncasanova <ethanjuan@yahoo.com>
2018-08-25 07:41:09 -0500

    added setState to the email,password

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -7,7 +7,7 @@ import "./Login.css";
 import API from "../../utils/API";
 import axios from "axios";
 
- 
+
 export default class Login extends Component {
 
   constructor(props) {
@@ -28,19 +28,19 @@ export default class Login extends Component {
       email: email,
       password: password
     })
-    .then((response) => {
-      this.setState({'redirect': `/customer/${response.data.user_id}`})
-      this.setState({'customer': response.data})
-      // this.checkIfLoggedIn();
-      this.setState({ isLoggedIn : true})
-    })
-    .catch(function (error) {
-      // display a state error message
-      alert("You have entered an invalid username or password")
-      console.log(error);
-    });
+      .then((response) => {
+        this.setState({ 'redirect': `/customer/${response.data.user_id}` })
+        this.setState({ 'customer': response.data })
+        // this.checkIfLoggedIn();
+        this.setState({ isLoggedIn: true })
+      })
+      .catch(function (error) {
+        // display a state error message
+        alert("You have entered an invalid username or password")
+        console.log(error);
+      });
   }
- 
+
 
 
 
@@ -50,7 +50,7 @@ export default class Login extends Component {
 
   handleChange = event => {
     this.setState({
-     [event.target.id]: event.target.value
+      [event.target.id]: event.target.value
     });
   }
 
@@ -58,11 +58,11 @@ export default class Login extends Component {
     event.preventDefault();
     API.getCustomerByEmail(this.state.email, this.state.password)
       // .then(res => this.setState({ customer: res.data }))
-    .then((res) => {
-      if (res.data !== null)
-        this.setState({ customer: res.data })
-    }
-    )
+      .then((res) => {
+        if (res.data !== null)
+          this.setState({ customer: res.data })
+      }
+      )
   }
 
   // componentDidUpdate() {
@@ -97,87 +97,91 @@ export default class Login extends Component {
   logOut = (response) => {
     console.log("logOut")
     axios.get('/api/logout', {
-   
+
     })
-    .then((response) => {
-      console.log(response)
-      this.setState({ isLoggedIn : false});
-      this.setState({'redirect': '/'})
-    })
-    .catch(function (error) {
-      // display a state error message
-      console.log(error);
-    });
+      .then((response) => {
+        console.log(response)
+        this.setState({
+          isLoggedIn: false,
+          email: "",
+          password: "",
+          'redirect': '/'
+        });
+      })
+      .catch(function (error) {
+        // display a state error message
+        console.log(error);
+      });
   }
 
 
   render() {
-   console.log("Logged in " + this.state.isLoggedIn )
+    console.log("Logged in " + this.state.isLoggedIn)
     if (this.state.isLoggedIn) {
-      
-      return <div >
-      <form className = " userLogIn form-inline"><h3> Welcome Back: {this.state.email}</h3>
-      <Button onClick={() => { this.logOut() }}>Log Out</Button>
-      {/* <ViewShoppingCart/> */}
-      </form>
-    </div>
-    
-    }else
 
-    {this.state.redirect && (
-      <Redirect to={this.state.redirect} />
-    )}
-    
+      return <div >
+        <form className=" userLogIn form-inline"><h3> Welcome Back: {this.state.email}</h3>
+          <Button onClick={() => { this.logOut() }}>Log Out</Button>
+          {/* <ViewShoppingCart/> */}
+        </form>
+      </div>
+
+    } else {
+      this.state.redirect && (
+        <Redirect to={this.state.redirect} />
+      )
+    }
+
     {
 
       return (
-        
+
 
         <div>
-        
+
           {this.state.redirect && (
             <Redirect to={this.state.redirect} />
           )}
-  
+
           <form className="form-inline" onSubmit={this.handleSubmit}>
-                    <FormGroup className = "m-1" controlId="email" bsSize="large">
-                      <ControlLabel>  Email  </ControlLabel>
-                      <FormControl
-                        autoFocus
-                        type="email"
-                        value={this.state.email}
-                        onChange={this.handleChange}
-                      />
-                </FormGroup>
-              <FormGroup className="m-1" controlId="password" bsSize="large">
-                      <ControlLabel>  Password  </ControlLabel>
-                      <FormControl
-                        value={this.state.password}
-                        onChange={this.handleChange}
-                        type="password"
-                      />
-              </FormGroup>
-              <FormGroup className="m-3">
-                    <Button onClick={() => { this.logIn(this.state.email, this.state.password,)} }
-                      block
-                      bsSize="large"
-                      disabled={!this.validateForm()}
-                    >
-                    
-                      Login
+            <FormGroup className="m-1" controlId="email" bsSize="large">
+              <ControlLabel>  Email  </ControlLabel>
+              <FormControl
+                autoFocus
+                type="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            <FormGroup className="m-1" controlId="password" bsSize="large">
+              <ControlLabel>  Password  </ControlLabel>
+              <FormControl
+                value={this.state.password}
+                onChange={this.handleChange}
+                type="password"
+              />
+            </FormGroup>
+            <FormGroup className="m-3">
+              <Button onClick={() => { this.logIn(this.state.email, this.state.password) }}
+                block
+                bsSize="large"
+                disabled={!this.validateForm()}
+              >
+
+                Login
                     </Button>
-               </FormGroup>
-              
-               <Link to="/profile/create"><Button className="btn btn-outline-success my-2 my-sm-0" type="submit">Create Account</Button></Link>
-               
-                  </form>
-        
+            </FormGroup>
+
+            <Link to="/profile/create"><Button className="btn btn-outline-success my-2 my-sm-0" type="submit">Create Account</Button></Link>
+
+          </form>
+
         </div>
       )
 
 
 
     }
-   
+
   }
 }
